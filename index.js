df1a25f88106e6469a96228a42fc8c44a992e372
Xin-Xin Wang - blueberry <xin-xinw@musiclifephilosophy.com>
2016-09-05 15:35:30 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var evh = require('express-vhost');
+
+app.set('port', (process.env.PORT || 8080));
+
+app.use(evh.vhost(app.enabled('trust proxy')));
+
+app.listen(app.get('port'), function (){
+  console.log('Listening on port ' + app.get('port') + '!');
+});
+
+var sites = require('./sites.json');
+
+sites.forEach(function (obj){
+  evh.register(obj.domain, require(obj.site));
+});
