ce93b422f3b1897f05d5cde7fa7e9acd9becc95f
Xin-Xin Wang - blueberry <xin-xinw@musiclifephilosophy.com>
2016-09-05 16:01:57 -0400

    add socket.io

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+global.io = require('socket.io')(http);
 var evh = require('express-vhost');
 
 app.set('port', (process.env.PORT || 8080));
